e89f1c3c4fd98acaaa2ab4a6196f9cc9c12362a7
k2archer <k2archer@hotmail.com>
2019-05-22 17:38:11 +0800

    添加 企业和账户信息 页面

---
+++ app/src/main/java/com/kwei/scentmerchant/activity/EnterpriseAccountDetailActivity.java
@@ -0,0 +1,15 @@
+package com.kwei.scentmerchant.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kwei.scentmerchant.R;
+
+public class EnterpriseAccountDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enterprise_account_detail);
+    }
+}
