b8e33bacac86c904ff96a0a78c5a65d99a78442d
k2archer <k2archer@hotmail.com>
2019-05-22 17:46:44 +0800

    添加跳转到 数据统计 页面

--- app/src/main/java/com/kwei/scentmerchant/activity/StickerIncomeActivity.java
+++ app/src/main/java/com/kwei/scentmerchant/activity/StickerIncomeActivity.java
@@ -1,15 +1,30 @@
 package com.kwei.scentmerchant.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.kwei.scentmerchant.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class StickerIncomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_sticker_income)
+    TextView tvStickerIncome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sticker_income);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.tv_sticker_income)
+    public void onViewClicked() {
+        startActivity(new Intent(this, StatisticsActivity.class));
     }
 }
