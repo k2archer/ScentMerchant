66fdcede9c11bae52d27b231b61a081ac3058b23
k2archer <k2archer@hotmail.com>
2019-05-31 18:52:27 +0800

    添加 Sticker 请求接口

--- app/src/main/java/com/kwei/scentmerchant/model/retrofit/ApiService.java
+++ app/src/main/java/com/kwei/scentmerchant/model/retrofit/ApiService.java
@@ -1,7 +1,8 @@
 package com.kwei.scentmerchant.model.retrofit;
 
-
 import com.kwei.scentmerchant.model.bean.BaseMessage;
+import com.kwei.scentmerchant.model.bean.StickerResponseBody;
+import com.kwei.scentmerchant.model.bean.base.BaseResponseBody;
 
 import java.util.List;
 
@@ -12,23 +13,26 @@ import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
-import retrofit2.http.Url;
 
 public interface ApiService {
     @POST("login")
     @FormUrlEncoded
-    Observable<BaseMessage> login(@Field("body")String body);
+    Observable<BaseMessage> login(@Field("body") String body);
 
     @POST("requestVerifyCode")
     @FormUrlEncoded
-    Observable<BaseMessage> requestVerifyCode(@Field("body")String body);
+    Observable<BaseMessage> requestVerifyCode(@Field("body") String body);
 
     @Multipart
     @POST("addNewShop")
     Observable<BaseMessage> addNewShop(@Part List<MultipartBody.Part> partList);
 
     @FormUrlEncoded
-    @POST()
-    Observable<BaseMessage> activateSticker(@Url String url, @Field("body") String body);
+    @POST("activeSticker")
+    Observable<BaseResponseBody> activateSticker(@Field("body") String body);
+
+    @POST("getStickerDetail")
+    @FormUrlEncoded
+    Observable<StickerResponseBody> getStickerDetail(@Field("body") String body);
 
 }
