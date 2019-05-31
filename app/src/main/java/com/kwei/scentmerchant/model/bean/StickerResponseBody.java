cf6b032f0359686c8c3ce3fd4e6792ce8b9e1fa6
k2archer <k2archer@hotmail.com>
2019-05-31 18:50:03 +0800

    添加 Sticker 请求响应 bean 类

---
+++ app/src/main/java/com/kwei/scentmerchant/model/bean/StickerResponseBody.java
@@ -0,0 +1,7 @@
+package com.kwei.scentmerchant.model.bean;
+
+import com.kwei.scentmerchant.model.bean.base.BaseResponseBody;
+
+public class StickerResponseBody extends BaseResponseBody {
+    public StickerDetail sticker;
+}
