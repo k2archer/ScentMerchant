223bfae07eb3fc0ce5f192de8fdedc71edbee39b
k2archer <k2archer@hotmail.com>
2019-05-31 18:47:54 +0800

    添加请求响应 bean 基类

---
+++ app/src/main/java/com/kwei/scentmerchant/model/bean/base/BaseResponseBody.java
@@ -0,0 +1,9 @@
+package com.kwei.scentmerchant.model.bean.base;
+
+    public class BaseResponseBody {
+    public String token;
+    public int code;
+    public String message;
+
+    public static int SUCCESS_CODE = 100200;
+}
