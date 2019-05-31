f50ffbf3b60092b01c10f8dac9d4ccdd9a3e47c5
k2archer <k2archer@hotmail.com>
2019-05-31 18:49:20 +0800

    添加 Sticker 请求 bean 类

---
+++ app/src/main/java/com/kwei/scentmerchant/model/bean/StickerRequestBody.java
@@ -0,0 +1,8 @@
+package com.kwei.scentmerchant.model.bean;
+
+
+import com.kwei.scentmerchant.model.bean.base.BaseRequestBody;
+
+public class StickerRequestBody extends BaseRequestBody {
+        public String id;
+}
