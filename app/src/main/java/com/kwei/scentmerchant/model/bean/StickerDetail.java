2670a5a57b6ea1a9257ffe6a93e8a042f957167e
k2archer <k2archer@hotmail.com>
2019-05-31 18:50:51 +0800

    添加 Sticker bean 类

---
+++ app/src/main/java/com/kwei/scentmerchant/model/bean/StickerDetail.java
@@ -0,0 +1,15 @@
+package com.kwei.scentmerchant.model.bean;
+
+import java.util.Date;
+
+public class StickerDetail {
+    public String id;      // ID
+    public int amount;     // 桌贴数量
+    public String content; // 桌贴内容
+    public Date date;      // 激活日期
+    public int cycle;      // 投放周期
+    public String model;   // 收益模式
+    public String settlement;  // 收益结算
+    public String scanMode;    // 扫码方式
+    public String[] pictures;  // 环境照
+}
