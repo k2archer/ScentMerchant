e33b79fc70bf325630ec4feeb2cde3ed48202c2d
k2archer <k2archer@hotmail.com>
2019-06-01 22:16:58 +0800

    添加 Response 状态值

--- app/src/main/java/com/kwei/scentmerchant/model/bean/base/BaseResponseBody.java
+++ app/src/main/java/com/kwei/scentmerchant/model/bean/base/BaseResponseBody.java
@@ -1,9 +1,11 @@
 package com.kwei.scentmerchant.model.bean.base;
 
-    public class BaseResponseBody {
+public class BaseResponseBody {
     public String token;
     public int code;
     public String message;
 
     public static int SUCCESS_CODE = 100200;
+    public static int FAIL_CODE = 100300;
+    public static int PARAMETER_ERROR_CODE = 100400;
 }
