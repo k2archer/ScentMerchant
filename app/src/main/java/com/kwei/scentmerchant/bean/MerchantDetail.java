4ad5b2b05f51eac38c8be08134acf9b93347fc91
k2archer <k2archer@hotmail.com>
2019-06-03 16:40:36 +0800

    添加 商家信息 bean 类

---
+++ app/src/main/java/com/kwei/scentmerchant/bean/MerchantDetail.java
@@ -0,0 +1,14 @@
+package com.kwei.scentmerchant.bean;
+
+public class MerchantDetail {
+    public String id;
+    public String accountType;  // 账户类型
+    public String name;
+    public String licenseCode;
+    public String bankName;
+    public String bankAccount;
+    public String state;        // 状态
+    public String[] license;    // 营业执照照片
+    public String[] permission; // 许可证照片
+
+}
