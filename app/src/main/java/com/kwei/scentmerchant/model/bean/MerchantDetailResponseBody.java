8762e6e3b3e9c4060db211550a86d0108ec4f621
k2archer <k2archer@hotmail.com>
2019-06-03 16:42:24 +0800

    添加 商家信息 请求响应 bean 类

---
+++ app/src/main/java/com/kwei/scentmerchant/model/bean/MerchantDetailResponseBody.java
@@ -0,0 +1,8 @@
+package com.kwei.scentmerchant.model.bean;
+
+import com.kwei.scentmerchant.bean.MerchantDetail;
+import com.kwei.scentmerchant.model.bean.base.BaseResponseBody;
+
+public class MerchantDetailResponseBody extends BaseResponseBody {
+    public MerchantDetail data;
+}
---
+++ app/src/main/java/com/kwei/scentmerchant/model/bean/MerchantSubmitRequestBody.java
@@ -0,0 +1,8 @@
+package com.kwei.scentmerchant.model.bean;
+
+
+import com.kwei.scentmerchant.model.bean.base.BaseRequestBody;
+
+public class MerchantSubmitRequestBody extends BaseRequestBody {
+        public String id;
+}
